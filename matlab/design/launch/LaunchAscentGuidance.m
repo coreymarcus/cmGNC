e831bf3f9b34220d136661cc01fdcfa3deb8ff2c
coreymarcus <corey.marcus@gmail.com>
2020-11-14 14:26:39 -0600

    Create LaunchAscentGuidance.m

---
+++ matlab/design/launch/LaunchAscentGuidance.m
@@ -0,0 +1,12 @@
+% This matlab script serves as a prototyping ground for developing my
+% launch ascent guidance system, it will be poorly documented, better
+% documentation for the final system will be found in the repositories
+% documentation
+
+%The general plan is to find a minimum fuel ascent trajactory which targets
+%some specified circular orbit. This will all be done through direct
+%optimization methods.
+
+clear
+close all
+clc
